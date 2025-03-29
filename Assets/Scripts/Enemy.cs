f80abf1cb136bdb6b590c1e07b7c8775b4f7a8f0
IKEALoophole <leotigerish@gmail.com>
2025-03-29 11:53:48 -0400

    Enemy buges is fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,12 @@ public class Enemy : MonoBehaviour
 {
 
     public int health = 1;
+
     public float moveSpeed = 1f;
 
     public bool moving = true;
 
-    public bool dashTime = 0f;
+    public float dashTime = 0f;
 
     public bool deadly = false;
 
@@ -28,14 +29,14 @@ public class Enemy : MonoBehaviour
     {
         transform = GetComponent<Transform>();
 
-        player = FindObjectOfType<Player>()
+        player = FindObjectOfType<Player>();
         playerTransform = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(MoveSpeed > 0 && player != null && moving)
+        if(moveSpeed > 0 && player != null && moving)
         {
             Move();
         }
@@ -47,18 +48,18 @@ public class Enemy : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
         if (rotateTowardsPlayer)
         {
-            Vector3 direction = player.position - transform.position;
+            Vector3 direction = playerTransform.position - transform.position;
             float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
 
             Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
 
-            transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime * rotationSpeed) LookAt(player);
+            transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime * rotationSpeed);
         }
     }
 
     public void Dash(Vector2 targetPos, float time, float speed)
     {
-        dashing
+
     }
 
     private void HandleDash()
