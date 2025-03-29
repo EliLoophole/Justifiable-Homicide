a068cd0e8cbd377d394d4c222323baadc651b2da
IKEALoophole <leotigerish@gmail.com>
2025-03-29 11:51:45 -0400

    Enemy Updates

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,18 @@ public class Enemy : MonoBehaviour
 {
 
     public int health = 1;
+    public float moveSpeed = 1f;
+
+    public bool moving = true;
+
+    public bool dashTime = 0f;
+
+    public bool deadly = false;
+
+    private float rotationSpeed = 1f;
+
+    public bool rotateTowardsPlayer = true;
+
     private Player player;
     private Transform playerTransform;
 
@@ -23,7 +35,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(MoveSpeed > 0)
+        if(MoveSpeed > 0 && player != null && moving)
         {
             Move();
         }
@@ -32,7 +44,30 @@ public class Enemy : MonoBehaviour
 
     public void Move()
     {
+        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+        if (rotateTowardsPlayer)
+        {
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
 
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime * rotationSpeed) LookAt(player);
+        }
+    }
+
+    public void Dash(Vector2 targetPos, float time, float speed)
+    {
+        dashing
+    }
+
+    private void HandleDash()
+    {
+        if (dashTime > 0f)
+        {
+            moving = false;
+            dashTime -= Time.deltaTime;
+        }
     }
 
 }
