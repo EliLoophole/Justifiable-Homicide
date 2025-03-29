85677b532dd7b516b9a240f3716db156c7e67561
IKEALoophole <leotigerish@gmail.com>
2025-03-29 12:33:44 -0400

    Enemy dash v1 added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,13 @@ public class Enemy : MonoBehaviour
     public int health = 1;
 
     public float moveSpeed = 1f;
+    public float dashSpeed = 0f;
 
     public bool moving = true;
 
-    public float dashTime = 0f;
+    private float dashTime = 0f;
+    private bool isDashing = false;
+    private Vector2 dashDirection;
 
     public bool deadly = false;
 
@@ -32,11 +35,14 @@ public class Enemy : MonoBehaviour
 
         player = FindObjectOfType<Player>();
         playerTransform = player.GetComponent<Transform>();
+
+        Dash(playerTransform.position, 1f, 10f);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(moveSpeed > 0 && player != null && moving)
         {
             Move();
@@ -58,18 +64,31 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public void Dash(Vector2 targetPos, float time, float speed)
+    IEnumerator Dash(Vector2 targetPos, float time, float speed)
     {
+        Vector2 pos = new Vector2 (transform.position.x,transform.position.y);
 
-    }
-
-    private void HandleDash()
-    {
-        if (dashTime > 0f)
+        dashDirection = targetPos - pos;
+        isDashing = true;
+        moving = false;
+        dashTime = time;
+        
+        while (dashTime > 0f)
         {
             moving = false;
             dashTime -= Time.deltaTime;
+            transform.Translate(dashDirection.normalized * dashSpeed * Time.deltaTime);
+
+            if (dashTime < 0f)
+            {
+                moving = true;
+                isDashing = false;
+                yield return null;
+            }
+
         }
+
+
     }
 
 }
